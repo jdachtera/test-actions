29340b36dc1d87967f846e539fc0c85f71a5a142
Jascha Dachtera <jascha@flowkey.com>
2019-10-18 16:51:46 +0200

    Add versionExists script

---
+++ scripts/versionExists.js
@@ -0,0 +1,15 @@
+const { name, version } = require("../package.json");
+
+const { exec } = require("child_process");
+
+const registry = process.argv[2] || "https://registry.npmjs.org/";
+
+exec(`npm search ${name} --json --registry ${registry}`, (error, stdout) => {
+  const results = JSON.parse(stdout);
+
+  const versionExists = results.find(
+    package => package.name === name && package.version === version
+  );
+
+  process.exit(versionExists ? 0 : 1);
+});
